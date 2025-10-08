e6224df1e8abda3b6612e6882669961667426d12
ChihiroGoto <tentere.pika-hino@ezweb.ne.jp>
2025-10-08 17:57:50 +0900

    Update github_connection.py

--- github_connection.py
+++ github_connection.py
@@ -1,7 +1,8 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Auto-deployed from + CodePipeline!!'
+        'body': 'Auto-deployed from + CodePipeline!!!!!'
     }
+
 
 
